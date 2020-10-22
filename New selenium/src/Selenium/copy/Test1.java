9ef7fc5baa1c92400e11a75331d81a0dae4f0bc1
avez raj <avez@codetoart.com>
2020-10-22 12:22:39 +0530

    Test 1

---
+++ New selenium/src/Selenium/copy/Test1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Selenium.copy;
+
+/**
+ * @author shirsathygita
+ *
+ */
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
